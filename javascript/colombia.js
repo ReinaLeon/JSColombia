e8c2c884de71796e120d48e761006c4cab9b32da
ReinaLeon <reinamorales922@gmail.com>
2021-12-13 20:09:00 -0500

    primer commit

---
+++ javascript/colombia.js
@@ -0,0 +1,149 @@
+const Departamento = () => {
+    let Departamento = prompt("Ingresa un departamento");
+
+
+
+    switch (Departamento) {
+
+        case "Amazonas":
+            alert("Leticia");
+            break;
+
+        case "Antioquia":
+            alert("Medellín");
+            break;
+
+        case "Arauca":
+            alert("Arauca");
+            break;
+
+        case "Atlántico":
+            alert("barranquilla");
+            break;
+
+        case "Bolívar":
+            alert("Cartagena");
+            break;
+
+        case "Boyacá":
+            alert("Tunja");
+            break;
+
+        case "Caldas":
+            alert("Manizales");
+            break;
+
+        case "Caquetá":
+            alert("Florencia");
+            break;
+
+        case "Casanare":
+            alert("Yopal");
+            break;
+
+        case "Cauca":
+            alert("Popayán");
+            break;
+
+        case "Cesar":
+            alert("Valledupar");
+            break;
+
+        case "Chocó":
+            alert("Quibdó");
+            break;
+
+        case "Córdoba":
+            alert("Montería");
+            break;
+
+        case "Cundinamarca":
+            alert("Bogotá");
+            break;
+
+        case "Guainía":
+            alert("Puerto InÍrida");
+            break;
+
+        case "Guajira":
+            alert("Riohacha");
+            break;
+
+        case "Guaviare":
+            alert("San José del Guaviare");
+            break;
+
+        case "Huila":
+            alert("Neiva");
+            break;
+
+        case "Magdalena":
+            alert("Santa Marta");
+            break;
+
+
+        case "Meta":
+            alert("Villavicencio");
+            break;
+
+        case "Nariño":
+            alert("Pasto");
+            break;
+
+        case "Norte de Santander":
+            alert("Cúcuta");
+            break;
+
+        case "Putumayo":
+            alert("Mocoa");
+            break;
+
+        case "Quindío":
+            alert("Armenia");
+            break;
+
+        case "Risaralda":
+            alert("Pereira");
+            break;
+
+        case "San Andrés y Providencia":
+            alert("San Andrés ");
+            break;
+
+        case "Santander":
+            alert("Bucaramanga");
+            break;
+
+        case "Sucre":
+            alert("Sincelejo");
+            break;
+
+        case "Tolima":
+            alert("Ibagué");
+            break;
+
+        case "Valle del Cauca":
+            alert("Cali");
+            break;
+
+        case "Vaupés":
+            alert("Mitú");
+            break;
+
+        case "Vichada":
+            alert("Puerto Carreño");
+            break;
+
+
+
+
+        default:
+            alert("Incorrecto por favor revisa la ortografía del departamento");
+            break;
+
+    }
+
+}
+
+
+
